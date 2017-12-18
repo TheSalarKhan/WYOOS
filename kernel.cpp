9d4e9a58b49506c9e96586584989755a5cde006d
Muhammad Salar Khan <lennylennylinuxlinux@gmail.com>
2017-12-18 22:21:32 +0500

    Implemented a very naive printf

--- kernel.cpp
+++ kernel.cpp
@@ -1,3 +1,13 @@
+void printf(const char* str) {
+	unsigned short* VIDEO_MEMORY = (unsigned short*)0xb8000;
+	/**
+	  Make sure that the string 'str' is null terminated
+	  or this loop will continue past the string.
+	 */
+	for(int i=0; str[i] != '\0'; i++) {
+		VIDEO_MEMORY[i] = (VIDEO_MEMORY[i]&0xFF00) | str[i];
+	}
+}
 
 extern "C" void kernel_main(const void* multiboot_struct, unsigned int magic_number)
 {
