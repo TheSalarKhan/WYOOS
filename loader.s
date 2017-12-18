3750eb91924fdb75c5cc95b24883876688c9bf32
Muhammad Salar Khan <lennylennylinuxlinux@gmail.com>
2017-12-18 21:29:57 +0500

    Added MAGIC number to the loader file

--- loader.s
+++ loader.s
@@ -1,3 +1,11 @@
+.set MAGIC, 0x1badb002
+.set FLAGS, (1<<0 | 1<<1)
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section multiboot
+	.long MAGIC
+	.long FLAGS
+	.long CHECKSUM
 
 .section .text
 .extern kernel_main
